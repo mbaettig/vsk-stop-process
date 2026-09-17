8918ba5569170aa212c07393ed41fdc5256b7799
Martin Bättig <martin.baettig@hslu.ch>
2026-09-17 10:39:00 +0200

    added changes for faster startup time

--- replit.nix
+++ replit.nix
@@ -1,15 +1,7 @@
-{ pkgs }:
-
-let
-  # Pinned nixpkgs (for JDK 25) – same result on every import
-  pkgsNew = import (builtins.fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/c7def046b9a883d46974757852106483d741586f.tar.gz") {};
-in
-{
+{ pkgs }: {
   deps = [
-    pkgsNew.jdk25
-    pkgsNew.maven
-    pkgsNew.jdt-language-server
+    pkgs.jdk21
+    pkgs.jdt-language-server
   ];
 }
 
